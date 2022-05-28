dc91a7f1128da6780e5568ebc7971e20c63d8bc8
Veronica_Lapunka <Veronica_Lapunka@epam.com>
2022-05-28 21:29:01 +0300

    Added method's realization.

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,60 @@
+import java.util.NoSuchElementException;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
+
     Resume[] storage = new Resume[10000];
+    int arraySize = 0;
 
     void clear() {
+        for (int i = 0; i < arraySize; i++) {
+            storage[i] = null;
+        }
+        arraySize = 0;
     }
 
     void save(Resume r) {
+        storage[arraySize] = r;
+        arraySize++;
     }
 
     Resume get(String uuid) {
+        for (int i = 0; i < arraySize; i++) {
+            if (storage[i].toString().equals(uuid)) {
+                return storage[i];
+            }
+        }
         return null;
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < arraySize; i++) {
+            if (storage[i].toString().equals(uuid)) {
+                storage[i] = null;
+                System.out.println("Element " + uuid + " was deleted");
+                for (int j = i; j < arraySize; j++) {
+                    storage[j] = storage[j+1];
+                }
+                arraySize--;
+                break;
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        Resume[] curStorage = new Resume[arraySize];
+        for (int i = 0; i < arraySize; i++) {
+            curStorage[i] = storage[i];
+        }
+        return curStorage;
     }
 
     int size() {
-        return 0;
+        return arraySize;
     }
 }
--- src/MainArray.java
+++ src/MainArray.java
@@ -13,10 +13,10 @@ public class MainArray {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         Resume r;
         while (true) {
-            System.out.print("Введите одну из команд - (list | size | save uuid | delete uuid | get uuid | clear | exit): ");
+            System.out.print("Please choose - (list | size | save uuid | delete uuid | get uuid | clear | exit): ");
             String[] params = reader.readLine().trim().toLowerCase().split(" ");
             if (params.length < 1 || params.length > 2) {
-                System.out.println("Неверная команда.");
+                System.out.println("Wrong command");
                 continue;
             }
             String uuid = null;
